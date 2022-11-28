74fbb4b8082216cb02847338178f83cc8774578d
Juan Briceño <bricenojuan297@gmail.com>
2022-11-27 20:09:46 -0500

    primer commit ejercicio

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+  
+    public static void main(String[] args) {
+        suma(2, 4, 6);
+
+        
+    }
+
+    public static int suma(int a, int b, int c) {
+        int resultado = 0;
+        resultado = a + b + c;
+        return resultado;
+        
+    }
+}
